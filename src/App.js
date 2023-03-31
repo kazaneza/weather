4e468b335e8e0c7629ad394f206c2f83fc09cf4a
kazaneza <kazanezagentil@gmail.com>
2023-03-31 16:54:57 +0100

    setting up

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>To do app</h1>
+      <div className ="todo-wrapper">
+        <div className='todo-input'>
+          <div className='todo-input-item'>
+            <label>Title</label>
+            <input type="text" placeholder="What's the task title?" />
+          </div>
+          <div className='todo-input-item'>
+            <label>Description</label>
+            <input type="text" placeholder="What's the task description?" />
+          </div>
+          <div className='todo-input-item'>
+            <button>Add</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
